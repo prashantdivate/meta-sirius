46703e5b3780823b20a520d1b0794441497e7a9b
Prashant Divate <diwateprashant44@gmail.com>
2021-11-20 17:44:28 +0530

    core: Add booting app script

---
+++ recipes-core/booting-app/booting-app-1.0/launch_user_program.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "########################################"
+echo "#                All done!             #"
+echo "# Add the app to start at booting time #"
+echo "########################################"
